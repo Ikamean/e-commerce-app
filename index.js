99ab741404aa24aace75aa9ce8cbaa9e71412fff
ikamean <ika.bidzishvili@gmail.com>
2021-03-26 10:15:34 +0400

    corrected path for index.js

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.get('/echo', (req, res) => {
 
 
 if( process.env.NODE_ENV === 'production' ) {
-    app.use(express.static( path.join ( __dirname, '/Client/build') ));
+    app.use(express.static( path.join ( __dirname, '/Client/e-commerce/build') ));
     
     app.get( '*' , (req, res) => {
-        res.sendFile(path.join( __dirname, 'Client', 'build', 'index.html' ));
+        res.sendFile(path.join( __dirname, 'Client', 'e-commerce', 'build', 'index.html' ));
     })
 };
 
